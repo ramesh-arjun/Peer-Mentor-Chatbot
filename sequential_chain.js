9b71a7e6602d1aede35f97a3ee49f979975fd2a5
Arjun R <103382747+ramesh-arjun@users.noreply.github.com>
2023-08-17 11:16:43 -0500

    Update sequential_chain.js

--- sequential_chain.js
+++ sequential_chain.js
@@ -22,21 +22,21 @@ Think carefully about all questions.
 text: {input}
 `;
 
-const reviewPromptTemplate1 = new PromptTemplate({
+const questionPromptTemplate1 = new PromptTemplate({
   template: responseTemplate1,
   inputVariables: ["input"],
 });
 
-const reviewPromptTemplate2 = new PromptTemplate({
+const questionPromptTemplate2 = new PromptTemplate({
   template: responseTemplate2,
   inputVariables: ["input"],
 });
 
-const reviewChain1 = new LLMChain({ llm, prompt: reviewPromptTemplate1 });
-const reviewChain2 = new LLMChain({ llm, prompt: reviewPromptTemplate2 });
+const questionChain1 = new LLMChain({ llm, prompt: questionPromptTemplate1 });
+const questionChain2 = new LLMChain({ llm, prompt: questionPromptTemplate2 });
 
 const overallChain = new SimpleSequentialChain({
-  chains: [reviewChain1, reviewChain2],
+  chains: [questionChain1, questionChain2],
   verbose: true,
 });
 
